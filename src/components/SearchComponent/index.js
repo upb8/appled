7bbee977021023f666d43d9e4894e93ea842fe43
Motasim Foad <motasimfoad@gmail.com>
2019-10-13 18:53:53 +0600

    Added background image

---
+++ src/components/SearchComponent/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './style.css';
+
+function Search() {
+  return (
+    <div className="main">
+     <p>
+       hello search component
+     </p>
+    </div>
+  );
+}
+
+export default Search;
---
+++ src/screens/Search/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './style.css';
+import SearchComponent from '../../components/SearchComponent'
+
+function Search() {
+  return (
+    <div className="main">
+      <div>
+        
+      </div>
+     <SearchComponent />
+    </div>
+  );
+}
+
+export default Search;
