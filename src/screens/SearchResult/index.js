10aa2364ed06528a94945fdb32ae9eb1fe120a48
Motasim Foad <motasimfoad@gmail.com>
2019-10-20 20:37:01 +0600

    result page : style fix 2

--- src/screens/SearchResult/index.js
+++ src/screens/SearchResult/index.js
@@ -47,7 +47,21 @@ function SearchResult() {
           Technical Details
         </h2>
         <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          <ul>
+
+          <li>Supports 9th and 8th Gen Intel® Core™ Processors</li>
+          <li>Supports 9th and 8th Gen Intel® Core™ Processors</li>
+          <li>Supports 9th and 8th Gen Intel® Core™ Processors</li>
+          <li>Supports 9th and 8th Gen Intel® Core™ Processors</li>
+          <li>Supports 9th and 8th Gen Intel® Core™ Processors</li>
+          <li>Supports 9th and 8th Gen Intel® Core™ Processors</li>
+          <li>Supports 9th and 8th Gen Intel® Core™ Processors</li>
+          <li>Supports 9th and 8th Gen Intel® Core™ Processors</li>
+          <li>Supports 9th and 8th Gen Intel® Core™ Processors</li>
+          <li>Supports 9th and 8th Gen Intel® Core™ Processors</li>
+          <li>Supports 9th and 8th Gen Intel® Core™ Processors</li>
+          <li>Supports 9th and 8th Gen Intel® Core™ Processors</li>
+          </ul>
         </p>
       </div>
     </div>
