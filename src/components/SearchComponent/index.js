2598540a0d61bdfab2ffb82f78ae0ca5ed9253e4
Upbit <upbitpc002@Upbits-iMac.local>
2019-10-22 21:02:17 +0600

    added auto-suggest

--- src/components/SearchComponent/index.js
+++ src/components/SearchComponent/index.js
@@ -10,6 +10,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import { withRouter } from 'react-router-dom';
 import image1 from '../../res/img/thumbs/e4200.jpg';
+import { style } from '@material-ui/system';
 
 // Imagine you have a list of languages that you'd like to autosuggest.
 const autosuggest = [
@@ -23,14 +24,14 @@ const autosuggest = [
   {
     image: image1,
     name: 'E4200',
-    component: "cpu",
+    component: "CPU",
     type: "Core 2 Duo",
     id: [2]
   },
   {
     image: '',
     name: 'E5200',
-    component: 'cpu',
+    component: 'CPU',
     type: 'i5',
     id: [4]
   },
@@ -82,8 +83,9 @@ const getSuggestions = value => {
 // When suggestion is clicked, Autosuggest needs to populate the input
 // based on the clicked suggestion. Teach Autosuggest how to calculate the
 // input value for every given suggestion.
-const getSuggestionValue = suggestion => suggestion.name;
-
+const getSuggestionValue = suggestion => {
+  return suggestion.type + " " + suggestion.name;
+}
 
 
 //this is the render function for suggestion
@@ -94,13 +96,24 @@ const renderSuggestion = suggestion => {
     <div>
       {
         suggestion.id.map((component, i) =>
-          <div key={component.toString()}>
-            <img src={suggestion.image} width="40" height="40"></img> - {suggestion.component} - {suggestion.type} - {suggestion.name}
-          </div>)
+          <div className="flex-container" key={component.toString()}>
+            <div>
+              <img src={suggestion.image} width="50" height="50"></img> 
+            </div>
+            <div className="auto-suggestion-text" key={component.toString()}> 
+              {suggestion.component} - {suggestion.type} {suggestion.name}
+            </div>
+          </div>
+          
+
+        )
       }
     </div>
   );
 };
+
+
+
  
 class Search extends React.Component {
   constructor() {
@@ -113,15 +126,22 @@ class Search extends React.Component {
     // and they are initially empty because the Autosuggest is closed.
     this.state = {
       value: '',
-      suggestions: []
+      suggestions: [],
+      baal: '',
     };
   }
 
   onChange = (event, { newValue }) => {
     this.setState({
       value: newValue
+      
+      
     });
   };
+
+   handleChange(e) {
+    console.log(e.target.value);
+  }
 
   // Autosuggest will call this function every time you need to update suggestions.
   // You already implemented this logic above, so just use it.
@@ -141,6 +161,7 @@ class Search extends React.Component {
   render() {
     const { value, suggestions } = this.state;
     console.log(this.props)
+    console.log(value);
 
 
     // Autosuggest will pass through all these props to the input.
@@ -176,6 +197,7 @@ class Search extends React.Component {
                   getSuggestionValue={getSuggestionValue}
                   renderSuggestion={renderSuggestion}
                   inputProps={inputProps}
+                  onChange={(e) => {this.handleChange(e)}}
                 />
               </div>
 
